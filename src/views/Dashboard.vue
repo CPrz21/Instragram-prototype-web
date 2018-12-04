fe5adcc6a1d3814c9983924d1d485742bbffe0fb
Carlos Perez <carlos.perez@grupoprobe.com>
2018-12-04 14:57:43 -0600

    facebook login component added

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="dashboard">
+    <facebook-login class="button"
+      appId="124451908218780"
+      @login="onLogin"
+      @logout="onLogout"
+      @sdk-loaded="sdkLoaded">
+    </facebook-login>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import facebookLogin from 'facebook-login-vuejs'
+
+export default {
+  name: "Dashboard",
+  components: {
+    facebookLogin
+  }
+};
+</script>
