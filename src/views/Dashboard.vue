0b6e2a019b51d419228e2880277f6986d785b901
Carlos Perez <carlos.perez@grupoprobe.com>
2018-12-04 15:31:38 -0600

    added login functions

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -15,8 +15,40 @@ import facebookLogin from 'facebook-login-vuejs'
 
 export default {
   name: "Dashboard",
-  components: {
-    facebookLogin
+  components: { facebookLogin },
+  data(){
+    // return{
+    //   idImage, loginImage, mailImage, faceImage,
+    //   isConnected: false,
+    //   name: '',
+    //   email: '',
+    //   personalID: '',
+    //   FB: undefined
+    // }
+  },
+  methods: {
+    getUserData() {
+      this.FB.api('/me', 'GET', { fields: 'id,name,email' },
+        userInformation => {
+          console.warn("data api",userInformation)
+          // this.personalID = userInformation.id;
+          // this.email = userInformation.email;
+          // this.name = userInformation.name;
+        }
+      )
+    },
+    sdkLoaded(payload) {
+      this.isConnected = payload.isConnected
+      this.FB = payload.FB
+      if (this.isConnected) this.getUserData()
+    },
+    onLogin() {
+      this.isConnected = true
+      this.getUserData()
+    },
+    onLogout() {
+      this.isConnected = false;
+    }
   }
 };
 </script>
