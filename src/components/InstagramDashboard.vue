f9b22064972235633169ebed98658ebd4f6ae3ab
Carlos Perez <carlos.perez@grupoprobe.com>
2018-12-06 22:53:31 -0600

    purple background to header

--- src/components/InstagramDashboard.vue
+++ src/components/InstagramDashboard.vue
@@ -1,14 +1,16 @@
 <template>
-  <div class="container mx-auto flex">
-    <div class="w-1/4 flex items-center justify-center">
-      <img class="rounded-full h-48" alt="Elaniin logo" :src='ElaniinLogo'>
-    </div>
-    <div class="w-3/4 flex items-center flex-wrap">
-      <div class="w-full">
-        <h1 class="w-full font-bold text-5xl">Elaniin</h1>
-        <MainWidgets title="100,000" subtitle="FOLLOWERS"/>
-        <MainWidgets title="1,000" subtitle="FOLLOWING"/>
-        <MainWidgets title="1,000" subtitle="MEDIA"/>
+  <div class="h-full">
+    <div class="w-full mx-auto flex px-12 bg-ix-purple dashboard-header">
+      <div class="w-1/4 flex items-center justify-center">
+        <img class="rounded-full h-48" alt="Elaniin logo" :src='ElaniinLogo'>
+      </div>
+      <div class="w-3/4 flex items-center flex-wrap">
+        <div class="w-full">
+          <h1 class="w-full font-bold text-5xl text-white">Elaniin</h1>
+          <MainWidgets title="100,000" subtitle="FOLLOWERS"/>
+          <MainWidgets title="1,000" subtitle="FOLLOWING"/>
+          <MainWidgets title="1,000" subtitle="MEDIA"/>
+        </div>
       </div>
     </div>
   </div>
