434b534d116405d38edaeef349e676f6026b4322
David Maravilla <bl4ckf4ll@gmail.com>
2018-12-10 16:05:09 -0600

    Update eslint

--- src/main.js
+++ src/main.js
@@ -1,25 +1,26 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
-import "./styles/css/tailwind.css";
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import './styles/css/tailwind.css';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import {
   faEye,
   faHandsHelping,
   faCaretLeft,
   faCaretRight,
-  faSignOutAlt} from '@fortawesome/free-solid-svg-icons';
+  faSignOutAlt,
+} from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
-library.add(faEye, faHandsHelping, faCaretLeft, faCaretRight, faSignOutAlt)
+library.add(faEye, faHandsHelping, faCaretLeft, faCaretRight, faSignOutAlt);
 Vue.component('font-awesome-icon', FontAwesomeIcon);
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
-  render: function(h) {
+  render(h) {
     return h(App);
-  }
-}).$mount("#app");
+  },
+}).$mount('#app');
