49cc21ea1c58231898dae5a578dcda9dff346331
Carlos Perez <carlos.perez@grupoprobe.com>
2018-12-12 10:44:52 -0600

    updated storyIcons component

--- src/components/dashboard/storieIcons.vue
+++ src/components/dashboard/storieIcons.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="w-full storie-insights-data flex items-center" :title="titleLabel">
     <font-awesome-icon class="storie-insights-icon h-8 p-2" :icon="iconName"/>
-    <p class="ml-3 font-medium">{{insightNumber}}</p>
+    <p class="ml-3 font-medium">{{insightNumber === -1 ? '0' : insightNumber}}</p>
   </div>
 </template>
 
