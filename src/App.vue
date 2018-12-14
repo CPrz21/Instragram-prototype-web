0a3c2f90f4ba8342ba15ea925d57727a9d1c4059
David Maravilla <bl4ckf4ll@gmail.com>
2018-12-14 10:20:22 -0600

    Update logout button style

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,8 @@
     <div id="app">
             <div id="nav" class="container mx-auto p-6">
             <ul class="list-reset flex">
-                <li class="mr-6">
-                    <router-link class="text-blue" to="/">Logout</router-link>
+                <li class="mr-6 p-4 bg-ix-blue rounded">
+                    <router-link class="text-white no-underline" to="/"><strong>Log</strong>out</router-link>
                 </li>
             </ul>
         </div>
