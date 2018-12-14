4b5619033ad7c87ca8bde3acb41db3f072335b35
David Maravilla <bl4ckf4ll@gmail.com>
2018-12-14 10:42:09 -0600

    update nav style

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
     <div id="app">
-            <div id="nav" class="container mx-auto p-6">
+        <div id="nav" class="container mx-auto p-6 bg-ix-purple">
             <ul class="list-reset flex">
                 <li class="mr-6 p-4 bg-ix-blue rounded">
                     <router-link class="text-white no-underline" to="/"><strong>Log</strong>out</router-link>
